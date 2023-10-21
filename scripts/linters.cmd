35de89fb5277d083258d00375f09ed7e83ca60c9
Alexander Sokol <sokol@compatibl.com>
2023-10-20 23:33:52 -0400

    Update scripts after conversion to namespace package.

--- scripts/linters.cmd
+++ scripts/linters.cmd
@@ -3,18 +3,8 @@
 pushd ..
 
 echo.
-echo Format using isort
-isort convince --sp=.isort.cfg
-isort tests --sp=.isort.cfg
-
-echo.
-echo Format using black
-black -q convince --config=pyproject.toml
-black -q tests --config=pyproject.toml
-
-echo.
 echo Validate using flake8
-flake8 convince --config=.flake8
+flake8 cl --config=.flake8
 flake8 tests --config=.flake8
 
 popd
