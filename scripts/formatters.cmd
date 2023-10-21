deba4960db6b878056d88a7428edddb360c1138a
Alexander Sokol <sokol@compatibl.com>
2023-10-21 14:28:05 -0400

    Add stubs and experiments directories to linter and formatter scripts.

--- scripts/formatters.cmd
+++ scripts/formatters.cmd
@@ -5,11 +5,15 @@ pushd ..
 echo.
 echo Format using isort
 isort cl --sp=.isort.cfg
+isort stubs --sp=.isort.cfg
 isort tests --sp=.isort.cfg
+isort experiments --sp=.isort.cfg
 
 echo.
 echo Format using black
 black -q cl --config=pyproject.toml
+black -q stubs --config=pyproject.toml
 black -q tests --config=pyproject.toml
+black -q experiments --config=pyproject.toml
 
 popd
--- scripts/linters.sh
+++ scripts/linters.sh
@@ -2,11 +2,12 @@
 
 cd ..
 
-# Validate using flake8
 echo ""
 echo "Validate using flake8"
 flake8 cl --config=.flake8
+flake8 stubs --config=.flake8
 flake8 tests --config=.flake8
+flake8 experiments --config=.flake8
 
 # Change back to the original directory
 cd -
